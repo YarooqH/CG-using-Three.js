5ddfe2df7bd1873a995bc59a7d719b08b7bd648c
Yarooq Anwar <yarooq1@gmail.com>
2022-08-06 23:36:37 +0500

    practice

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 // import * as THREE from 'three';
 import * as THREE from 'three';
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
+
+// const dat = require('dat.gui');
 // import { OrbitControls } from 'OrbitControls.js';
+
+// import * from 'three-dat.gui'; // Import initialization method
+// init(Dat);
+// var gui = new Dat.GUI();
 
 
 const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -25,25 +31,53 @@ const material = new THREE.MeshBasicMaterial( { color: '#f3da69' });
 const cube = new THREE.Mesh( geometry, material );
 // scene.add( cube );
 
-const mat = new THREE.LineBasicMaterial({ color: 'pink' });
-const points = [];
-// points.push(new THREE.Vector2(-, 0));
-points.push(new THREE.Vector2(-5,-10));
-points.push(new THREE.Vector2(-5, 10));
-points.push(new THREE.Vector2(5, -10));
-points.push(new THREE.Vector2(5, 10));
+// const mat = new THREE.LineBasicMaterial({ color: 'pink' });
+// const points = [];
+// // points.push(new THREE.Vector2(-, 0));
+// points.push(new THREE.Vector2(-5,-10));
+// points.push(new THREE.Vector2(-5, 10));
+// points.push(new THREE.Vector2(5, -10));
+// points.push(new THREE.Vector2(5, 10));
 
-const geo = new THREE.BufferGeometry().setFromPoints(points);
+// const geo = new THREE.BufferGeometry().setFromPoints(points);
 
-const line = new THREE.Line(geo, mat);
-scene.add(line);
+// const line = new THREE.Line(geo, mat);
+// scene.add(line);
 
 // const cloud = new THREE.Group();
+const cloudClr = '#f3da69';
+
+// gui.addMaterial('material', cloudClr);
 
 const cloudGeometry = new THREE.CircleGeometry( 0.5, 32 );
-const cloudMaterial = new THREE.MeshBasicMaterial( { color: '#f3da69' });
+const cloudMaterial = new THREE.MeshBasicMaterial( { color: cloudClr });
 const cloud = new THREE.Mesh( cloudGeometry, cloudMaterial );
-scene.add( cloud );
+// scene.add( cloud );
+
+const minicloud1 = new THREE.Mesh(
+  new THREE.CircleGeometry( 0.52, 32),
+  new THREE.MeshBasicMaterial({ color: cloudClr })
+);
+minicloud1.position.set(0.5,0.1,0);
+
+const minicloud2 = new THREE.Mesh(
+  new THREE.CircleGeometry( 0.52, 32),
+  new THREE.MeshBasicMaterial({ color: cloudClr })
+);
+minicloud2.position.set(1,0.05,0);
+
+const minicloud3 = new THREE.Mesh(
+  new THREE.CircleGeometry( 0.52, 32),
+  new THREE.MeshBasicMaterial({ color: cloudClr })
+)
+minicloud3.position.set(1.5,0.05,0);
+
+const cloudGrp = new THREE.Group();
+cloudGrp.add(minicloud1, minicloud2, minicloud3,cloud);
+scene.add(cloudGrp);
+cloudGrp.position.set(-3,0,0);
+
+
 
 
 // const geometry1 = new THREE.CircleGeometry( 1, 32 );
@@ -60,7 +94,7 @@ scene.add( cloud );
 // scene.add(obj)
 
 
-camera.position.z = 20;
+camera.position.z = 10;
 
 function animate() {
   requestAnimationFrame( animate );
